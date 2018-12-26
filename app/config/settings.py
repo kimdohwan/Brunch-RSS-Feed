90c536779b64022bbad7425998491f05441a9289
kimdohwan <dosio0102@gmail.com>
2018-12-26 18:20:50 +0900

    members.User makemigrations/migrate

--- app/config/settings.py
+++ app/config/settings.py
@@ -27,10 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = 'members.User'
 # Application definition
 
 INSTALLED_APPS = [
+    'members',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
