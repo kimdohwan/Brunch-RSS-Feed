e18c4c55239739990e09e8ccf9a65b9112b06903
kimdohwan <dosio0102@gmail.com>
2018-12-28 16:40:04 +0900

    readme 에 프로젝트 개요 작성

--- app/config/views.py
+++ app/config/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    # return HttpResponse('안녕')
     context = {
         'my_name': 'doh'
     }
     return render(request, 'index.html', context=context)
+    # return HttpResponse('안녕')
