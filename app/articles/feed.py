63646c722e64f9b0e868e22adf8ae0bad26aca38
kimdohwan <dosio0102@gmail.com>
2019-01-01 03:41:38 +0900

    Add Feed Generator(example)
    models.py
    	Create Posts
    
    feed.py
    	Create post_detail(), TestFeed
    
    urls.py
    	Add articles(config)
    	Create feeds, post-detail(articles)
    
    templates
    	Create post-detail.html

---
+++ app/articles/feed.py
@@ -0,0 +1,31 @@
+from django.contrib.syndication.views import Feed
+from django.shortcuts import render
+from django.urls import reverse
+
+from articles.models import Posts
+
+
+def post_detail(request, pk):
+    post = Posts.objects.get(pk=pk)
+    context = {
+        'post': post
+    }
+    return render(request, 'post-detail.html', context=context)
+
+
+class TestFeed(Feed):
+    title = "Posts for bedjango starter"
+    link = "/feeds/"
+    description = "Updates on changes and additions to posts published in the starter."
+
+    def items(self):
+        return Posts.objects.order_by('name')[:5]
+
+    def item_title(self, item):
+        return item.name
+
+    def item_description(self, item):
+        return item.content
+
+    def item_link(self, item):
+        return reverse('post-detail', args=[item.pk])
