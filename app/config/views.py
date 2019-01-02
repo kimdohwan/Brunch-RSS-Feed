e8f30343f3276d8256f9e7201166bf79c8f42759
kimdohwan <dosio0102@gmail.com>
2019-01-03 00:09:46 +0900

    Modify

--- app/config/views.py
+++ app/config/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render
 
-from articles.models import Article, Posts
+from articles.models import Post
 from members.models import User
 
 
 def index(request):
     user_all = User.objects.all()
-    article_all = Article.objects.all()
-    post_all = Posts.objects.all()
+    post_all = Post.objects.all()
     context = {
         'my_name': 'doh',
         'user_all': user_all,
-        'article_all': article_all,
         'post_all': post_all
     }
 
