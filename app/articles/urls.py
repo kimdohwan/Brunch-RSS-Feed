90a504c723f14930b91a7f21ba4e2ee7299d6cec
kimdohwan <dosio0102@gmail.com>
2019-01-03 00:08:53 +0900

    Modify feeds/<keyword>/

--- app/articles/urls.py
+++ app/articles/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from articles.feed import TestFeed, post_detail
 
 urlpatterns = [
-    path('posts/<pk>', post_detail, name='post-detail'),
-    path('feeds/', TestFeed(), name='feeds')
+    path('posts/<pk>/', post_detail, name='post-detail'),
+    path('feeds/<keyword>/', TestFeed(), name='feeds')
 ]
