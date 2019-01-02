7f5bb1b867bfec0e5b6aa7a559e98c2948ea5fc3
kimdohwan <dosio0102@gmail.com>
2019-01-02 16:11:57 +0900

    Modify index template

--- app/config/views.py
+++ app/config/views.py
@@ -1,18 +1,20 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-from articles.models import Article
+from articles.models import Article, Posts
 from members.models import User
 
 
 def index(request):
-    u = User.objects.all()
-    a = Article.objects.all()
+    user_all = User.objects.all()
+    article_all = Article.objects.all()
+    post_all = Posts.objects.all()
     context = {
         'my_name': 'doh',
-        'u': u,
-        'a': a,
+        'user_all': user_all,
+        'article_all': article_all,
+        'post_all': post_all
     }
 
     return render(request, 'index.html', context=context)
-    # return HttpResponse('안녕')
+
+
