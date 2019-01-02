6070ab1d86eeec7c08b9b01b22160a348c6d7d5c
kimdohwan <dosio0102@gmail.com>
2019-01-03 00:08:06 +0900

    Delete Article, Modify Post

--- app/articles/models.py
+++ app/articles/models.py
@@ -3,12 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-class Article(models.Model):
-    uid = models.CharField(max_length=100)
-    article_no = models.IntegerField()
-    content = models.TextField()
-
-
-class Posts(models.Model):
-    name = models.CharField(verbose_name='Name', null=False, max_length=50)
-    content = models.CharField(verbose_name='Content', null=False, max_length=300)
+class Post(models.Model):
+    title = models.CharField(verbose_name='제목', null=False, max_length=100)
+    content = models.CharField(verbose_name='본문', null=False, max_length=300)
+    keyword = models.CharField(verbose_name='키워드', null=False, max_length=50)
