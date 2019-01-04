eb26eb57b7a7257947e62bc1a338f04c774d05a1
kimdohwan <dosio0102@gmail.com>
2019-01-04 15:59:46 +0900

    인덱스 페이지 템플릿 수정 중

--- app/config/urls.py
+++ app/config/urls.py
@@ -16,12 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from articles.feed import TestFeed
-from .views import index
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', include('articles.urls')),
     path('admin/', admin.site.urls),
-
-    path('articles/', include('articles.urls')),
 ]
