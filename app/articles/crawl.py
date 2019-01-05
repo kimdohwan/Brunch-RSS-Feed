b2cf4b30dac0a7a48a24f688d6668b5f6cbd03d2
kimdohwan <dosio0102@gmail.com>
2019-01-06 03:44:45 +0900

    asyncio 비동기화 크롤링

--- app/articles/crawl.py
+++ app/articles/crawl.py
@@ -1,12 +1,15 @@
+import asyncio
 import os
+import re
 import time
+from functools import partial
 
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import ElementNotVisibleException
 
-from .models import Article, Keyword
+from .models import Article, Keyword, Writer
 from config.settings import BASE_DIR
 
 
@@ -65,7 +68,7 @@ def get_href_to_detail(html):
 def save_article(href_list, keyword):
     href_list = href_list[:3]  # 테스트용, 3개만 긁자.
 
-    obj_keyword, created = Keyword.objects.get_or_create(search_word=keyword)
+    obj_keyword, created = Keyword.objects.get_or_create(keyword=keyword)
     new_href_list = [new_href for new_href in href_list]
 
     # 이미 존재(저장되어있는)하는 포스트라면 항목 제거
@@ -74,19 +77,91 @@ def save_article(href_list, keyword):
         if obj_article:
             new_href_list.remove(href)
             # 포스트가 현재 검색된 키워드를 가지지 않았다면 키워드는 추가시켜준다
-            if not obj_article[0].keyword.filter(search_word=keyword):
+            if not obj_article[0].keyword.filter(keyword=keyword):
                 obj_article[0].keyword.add(obj_keyword)
                 obj_article[0].save()
 
-    # 해당 포스트 페이지 방문 후 크롤링 및 저장
-    brunch_url = 'https://brunch.co.kr'
-    for href in new_href_list:
-        url = brunch_url + href
-        r = requests.get(url)
+    s = time.time()
+
+    # # 해당 포스트 페이지 방문 후 크롤링 및 저장
+    # brunch_url = 'https://brunch.co.kr'
+    # for href in new_href_list:
+    #     url = brunch_url + href
+    #     r = requests.get(url)
+    #
+    #     soup = BeautifulSoup(r.text, 'lxml')
+    #     title = soup.select_one('div.cover_cell').text
+    #     content = soup.select_one('div.wrap_body').prettify()
+    #     media_name = soup.find('meta', {'name': 'article:media_name'})['content']
+    #     published_time = re.findall(
+    #         r'(\S+)\+',
+    #         soup.find('meta', {'property': 'article:published_time'})['content'],
+    #     )[0]
+    #     user_id, text_id = re.findall(
+    #         r'/@(\S+)/(\d+)',
+    #         soup.find('meta', {'property': 'og:url'})['content']
+    #     )[0]
+    #
+    #     writer, _ = Writer.objects.get_or_create(
+    #         user_id=user_id,
+    #         media_name=media_name
+    #     )
+    #
+    #     article_without_keyword = Article.objects.create(
+    #         title=title,
+    #         content=content,
+    #         href=href,
+    #         published_time=published_time,
+    #         text_id=text_id,
+    #         writer=writer
+    #     )
+    #     article_without_keyword.keyword.add(obj_keyword)
+
+    async def test_async(url, new_href):
+        print(f'Send request .. {url}')
+        loop = asyncio.get_event_loop()
+        r = await loop.run_in_executor(None, requests.get, url)
+        print(f'Get response .. {url}')
 
         soup = BeautifulSoup(r.text, 'lxml')
         title = soup.select_one('div.cover_cell').text
         content = soup.select_one('div.wrap_body').prettify()
+        media_name = soup.find('meta', {'name': 'article:media_name'})['content']
+        published_time = re.findall(
+            r'(\S+)\+',
+            soup.find('meta', {'property': 'article:published_time'})['content'],
+        )[0]
+        user_id, text_id = re.findall(
+            r'/@(\S+)/(\d+)',
+            soup.find('meta', {'property': 'og:url'})['content']
+        )[0]
 
-        article_without_keyword = Article.objects.create(title=title, content=content, href=href)
+        writer, _ = Writer.objects.get_or_create(
+            user_id=user_id,
+            media_name=media_name
+        )
+        article_without_keyword = Article.objects.create(
+            title=title,
+            content=content,
+            href=new_href,
+            published_time=published_time,
+            text_id=text_id,
+            writer=writer
+        )
         article_without_keyword.keyword.add(obj_keyword)
+
+        print(f'저장 완료 {url}')
+
+    async def test_main():
+        brunch_url = 'https://brunch.co.kr'
+        futures = [asyncio.ensure_future(test_async(
+            brunch_url + new_href, new_href)) for new_href in new_href_list]
+
+        await asyncio.gather(*futures)
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(test_main())
+
+    e = time.time()
+    print('상세페이지 크롤링 시간', e - s)
--- app/articles/tests.py
+++ app/articles/tests.py
@@ -1,22 +1,7 @@
-from django.test import TestCase
-import asyncio
+import re
 
-from articles.crawl import crawl
-# crawl('아')
-# save_article()
+from bs4 import BeautifulSoup
 
-import time
+from .crawl import crawl
 
-
-async def sleep():
-    await asyncio.sleep(2)
-
-
-start = time.time()
-# 이벤트 루프 정의
-loop = asyncio.get_event_loop()
-# 이벤트 루프 실행
-loop.run_until_complete(sleep())
-end = time.time()
-
-print(str(end - start) + 's')
+# crawl('오잉')
