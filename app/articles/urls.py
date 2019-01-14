4a2dd25fd6c1cab16b968b8f087a08976b8cfe0b
kimdohwan <dosio0102@gmail.com>
2019-01-14 16:53:03 +0900

    url 변경 static

--- app/articles/urls.py
+++ app/articles/urls.py
@@ -12,4 +12,4 @@ urlpatterns = [
     path('detail/<pk>/', article_detail, name='article-detail'),
     path('feeds/keyword/<keyword>/', KeywordFeed(), name='feeds-keyword'),
     path('feeds/writer/<user_id>/', WriterFeed(), name='feeds-writer'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]  # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
