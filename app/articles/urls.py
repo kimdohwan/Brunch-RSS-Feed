d2904b8d6846ff5863de4889f0ce54d0f8967fd3
kimdohwan <dosio0102@gmail.com>
2019-01-14 14:37:39 +0900

    static url 추가

--- app/articles/urls.py
+++ app/articles/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from .feed import KeywordFeed, WriterFeed, MyFeed
@@ -10,4 +12,4 @@ urlpatterns = [
     path('detail/<pk>/', article_detail, name='article-detail'),
     path('feeds/keyword/<keyword>/', KeywordFeed(), name='feeds-keyword'),
     path('feeds/writer/<user_id>/', WriterFeed(), name='feeds-writer'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
