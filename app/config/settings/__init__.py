f18dd56eaac45c25e201918c9f69ba2e687c00a3
kimdohwan <dosio0102@gmail.com>
2019-03-11 02:37:56 +0900

    DJANGO_SETTINGS_MODULE 설정 없을 때는 local 로 기본 셋팅하도록 설정

--- app/config/settings/__init__.py
+++ app/config/settings/__init__.py
@@ -6,4 +6,9 @@
 #     RDS, S3 를 사용, Debug=True
 # settings.production
 #     실제 배포 환경, Debug=False
-from .local import *
+import os
+
+DJANGO_SETTINGS_MODULE = os.environ.get('DJANGO_SETTINGS_MODULE')
+print(DJANGO_SETTINGS_MODULE)
+if not DJANGO_SETTINGS_MODULE or DJANGO_SETTINGS_MODULE == 'config.settings':
+    from .local import *
