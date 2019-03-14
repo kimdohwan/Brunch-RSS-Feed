2c81b8e1e0e78bcb0eb000bd693b1b792748325c
kimdohwan <dosio0102@gmail.com>
2019-03-14 13:56:02 +0900

    eb deploy 를 쉘 스크립트로 자동화

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+pipenv lock -r > requirements.txt
+echo " Create requirements.txt"
+
+git add requirements.txt
+echo " Git Add requirements.txt"
+
+git add .secrets -f
+echo " Git Add secrets"
+
+git add --all
+echo " Git Add All"
+
+eb deploy --profile doh-brunch-EB --staged
+echo " Eb deploy"
+
+git reset HEAD .secrets/ requirements.txt
+echo " Git Reset secrets, requirements.txt"
+
+rm requirements.txt
+echo " Rm requirements.txt"
+
+eb open
+echo " Open eb"
