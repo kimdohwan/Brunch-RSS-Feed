7d4a4f333ed8a9e1b52a636393304205704a9e41
kimdohwan <dosio0102@gmail.com>
2019-03-14 13:50:58 +0900

    Modify ALLOWED_HOST/ Remove STATIC_ROOT, STATIC_URL/ Add LOGGING
    - ALLOWED_HOST 를 .secrets 에서 불러오도록 설정
    
    - S3 사용할 때 필요없는 STATIC_ROOT 삭제, STATIC_URL 은 .base.py 로 이동
    
    - LOGGING 설정을 추가해 eb cli docker 에서 장고 에러로그를 확인 할 수 있도록 설정
    	- runserver 로 동작할 시 LOGGING 설정 동작하지 않음

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -1,13 +1,11 @@
+import sys
+
 from .base import *
 
 secrets = json.load(open(os.path.join(SECRET_DIR, 'production.json')))
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '.amazonaws.com',
-]
+ALLOWED_HOSTS = secrets['ALLOWED_HOSTS']
 
 # production 에서만 사용되는 app
 INSTALLED_APPS += [
@@ -18,13 +16,33 @@ INSTALLED_APPS += [
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'config.storages.S3StaticStorage'
 
-# Static
-STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
-STATIC_URL = '/static/'
-
 # WSGI(이곳에는 적어주지 않아도 잘 동작한다.
 # 아마도 request 를 받을 때는 wsgi -> settings 방향이고 respond 나 다른 경우는 반대 방향으로 추측된다
 WSGI_APPLICATION = 'config.wsgi.production.application'
 
 # DB
 DATABASES = secrets['DATABASES']
+
+# runserver 테스트 할 떄는 로그파일 생성되지 않도록 설정
+if 'runserver' not in sys.argv:
+    # eb docker 환경에서 장고 에러를 기록하는 파일 생성
+    LOG_FILE_PATH = '/var/log/django_error.log'
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'logfile': {
+                'class': 'logging.handlers.RotatingFileHandler',
+                'filename': LOG_FILE_PATH,
+                'maxBytes': 10485760,
+                'backupCount': 10,
+            }
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['logfile'],
+                'level': 'ERROR',
+                'propagate': False,
+            },
+        },
+    }
