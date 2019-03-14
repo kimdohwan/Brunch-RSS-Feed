4bb4d6a01067107efedb938f4564dc1402d62d1b
kimdohwan <dosio0102@gmail.com>
2019-03-14 13:48:33 +0900

    ALLOWED_HOST 설정을 .secrets 로 변경, STATIC 설정 삭제(S3 에 필요없는 설정 또는 base.py 로 변경)

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -3,11 +3,7 @@ from .base import *
 secrets = json.load(open(os.path.join(SECRET_DIR, 'dev.json')))
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '.amazonaws.com',
-]
+ALLOWED_HOSTS = secrets['ALLOWED_HOSTS']
 
 # dev 에서만 사용되는 app
 INSTALLED_APPS += [
@@ -18,10 +14,6 @@ INSTALLED_APPS += [
 # static file 을 aws S3 를 통해 불러오게 한다.
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'config.storages.S3StaticStorage'
-
-# Static
-STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
-STATIC_URL = '/static/'
 
 # WSGI(이곳에는 적어주지 않아도 잘 동작한다.
 # 아마도 request 를 받을 때는 wsgi -> settings 방향이고 respond 나 다른 경우는 반대 방향으로 추측된다
