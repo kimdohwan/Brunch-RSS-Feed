5ca2989aec62ec32082b74dff2a449d55d2eef0d
kimdohwan <dosio0102@gmail.com>
2019-04-17 20:15:01 +0900

    [FIX] search() to create_feed_url()

--- app/articles/urls.py
+++ app/articles/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from .feeds import KeywordFeed, WriterFeed
-from .views import article_detail, index, search
+from .views import article_detail, index, create_feed_url
 
 app_name = 'articles'
 urlpatterns = [
     path('', index, name='index'),
-    path('search/', search, name='search'),
+    path('search/', create_feed_url, name='search'),
     path('detail/<pk>/', article_detail, name='article-detail'),
     path('feeds/keyword/<keyword>/', KeywordFeed(), name='feeds-keyword'),
     path('feeds/writer/<user_id>/', WriterFeed(), name='feeds-writer'),
