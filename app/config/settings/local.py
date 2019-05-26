463a5aa1d6d3ae900d1ffa4244b1e037d15e82e7
kimdohwan <dosio0102@gmail.com>
2019-05-26 20:44:46 +0900

    [ADD] celery 설정 추가

--- app/config/settings/local.py
+++ app/config/settings/local.py
@@ -25,4 +25,16 @@ DATABASES = {
 # local 에서만 사용되는 app
 INSTALLED_APPS += [
     'django_extensions',
+    'django_celery_beat',
+    'django_celery_results',
 ]
+
+# CELERY + Redis
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+# # CELERY_BEAT
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TAST_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Seoul' # Celery beat가 스케줄러이기 때문에 시간에 대한 정의를 해야함
