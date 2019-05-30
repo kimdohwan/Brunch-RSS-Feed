f84f273266cb28418ba5e10e0b40d4793cbd5a46
kimdohwan <dosio0102@gmail.com>
2019-05-30 19:00:29 +0900

    [FIX] 크롤링 중 반환 될 피드 객체 처리

--- app/articles/feeds/keyword.py
+++ app/articles/feeds/keyword.py
@@ -9,7 +9,17 @@ class KeywordFeed(MyFeed):
     description = "등록한 검색어의 최신글들을 업데이트합니다"
 
     def items(self, keyword):  # Keyword 에 연결된(ManyToMany) Article
-        return Keyword.objects.get(keyword=keyword).articles.all().order_by('-published_time')
+
+        try:
+            query_set = Keyword.objects.get(keyword=keyword).articles.all().order_by('-published_time')
+
+        # 크롤링 작업을 모두 마치지 못한 상태에서 피드를 호출할 경우
+        # 빈 리스트를 리턴 한 후, 피드 생성 중이라는 메시지를 Feed 의 title 에 셋팅(Myfeed)
+        except Keyword.DoesNotExist:
+            query_set = []
+
+        return query_set
+        # return Keyword.objects.get(keyword=keyword).articles.all().order_by('-published_time')
 
     def get_object(self, request, *args, **kwargs):
         return str(kwargs['keyword'])  # 검색된 키워드 전달
--- app/articles/feeds/writer.py
+++ app/articles/feeds/writer.py
@@ -9,7 +9,16 @@ class WriterFeed(MyFeed):
     description = "등록한 작가의 최신글들을 업데이트합니다"
 
     def items(self, user_id):  # Writer 에 연결된(Foreign key) Article
-        return Writer.objects.get(user_id=user_id).articles.all().order_by('-published_time')
+
+        try:
+            query_set = Writer.objects.get(user_id=user_id).articles.all().order_by('-published_time')
+
+        # 크롤링 작업을 모두 마치지 못한 상태에서 피드를 호출할 경우
+        # 빈 리스트를 리턴 한 후, 피드 생성 중이라는 메시지를 Feed 의 title 에 셋팅(Myfeed)
+        except Writer.DoesNotExist:
+            query_set = []
+
+        return query_set
 
     def get_object(self, request, *args, **kwargs):
         return str(kwargs['user_id'])  # 검색된 작가id 전달
