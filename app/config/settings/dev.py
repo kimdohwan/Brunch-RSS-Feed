4aae3584f45c1377f6baab382e0d851c28424d5e
kimdohwan <dosio0102@gmail.com>
2019-05-31 20:35:10 +0900

    [ADD] sentry 설정 및 로깅 파일

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -1,3 +1,10 @@
+import sys
+
+import sentry_sdk
+from sentry_sdk.integrations.celery import CeleryIntegration
+from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.wsgi import SentryWsgiMiddleware
+
 from .base import *
 
 secrets = json.load(open(os.path.join(SECRET_DIR, 'dev.json')))
@@ -33,3 +40,43 @@ CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TAST_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Asia/Seoul' # Celery beat가 스케줄러이기 때문에 시간에 대한 정의를 해야함
+
+# Sentry
+sentry_sdk.init(
+    dsn='https://8ba8c02a3fa2415198d88455ff7fd6b9@sentry.io/1472002',
+    integrations=[
+        DjangoIntegration(),
+        CeleryIntegration(),
+        # AioHttpIntegration(),  python version 3.7 이상 지원하는 기능, 추후 적용해 볼것
+    ]
+)
+
+# 로컬에서 production 환경의 runserver, shell_plus 실행 시 로깅파일 생성 안되도록 설정
+local_command = ['runserver', 'shell_plus', 'shell']
+for command in local_command:
+    if command in sys.argv:
+        print(command)
+        break
+
+else:
+    # docker 에서 장고 에러를 기록하는 파일 생성
+    LOG_FILE_PATH = '/var/log/django_error.log'
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'logfile': {
+                'class': 'logging.handlers.RotatingFileHandler',
+                'filename': LOG_FILE_PATH,
+                'maxBytes': 10485760,
+                'backupCount': 10,
+            }
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['logfile'],
+                'level': 'ERROR',
+                'propagate': False,
+            },
+        },
+    }
