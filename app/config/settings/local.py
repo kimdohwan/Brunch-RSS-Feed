39ecf9c49ac6a916be258e6f1c4444469e6cc948
kimdohwan <dosio0102@gmail.com>
2019-05-31 16:58:45 +0900

    [FIX] local 환경에서 docker compose redis 사용 안함

--- app/config/settings/local.py
+++ app/config/settings/local.py
@@ -30,8 +30,8 @@ INSTALLED_APPS += [
 ]
 
 # CELERY + Redis
-CELERY_BROKER_URL = 'redis://redis:6379/0'
-CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 # # CELERY_BEAT
 CELERY_ACCEPT_CONTENT = ['application/json']
